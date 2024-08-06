4d8ad8991ebf38a790288fb301555a0f805ace1e
Abhishek Katnoria <abhishekkatnoria993@gmail.com>
2024-08-07 00:05:07 +0530

    Added the side image for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,14 +10,30 @@ export default function Home() {
           <Image
             width={100}
             height={100}
-            alt=""
+            alt="Logo"
             src="/assets/icons/logo-full.svg"
             className="w-fit h-10 mb-12"
+            loading="lazy"
           />
 
           <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="text-dark-600">@carepulse copyright</p>
+            <Link className="text-green-500" href="/?admin=true">
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
+      <Image
+        width={1000}
+        height={1000}
+        alt="Home Image"
+        src="/assets/images/onboarding-img.png"
+        className="side-img max-w-[50%]"
+        loading="lazy"
+      />
     </div>
   );
 }
