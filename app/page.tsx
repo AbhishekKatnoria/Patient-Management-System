ff4ab1b82029a01f7820c8b4945ed72c01a44b4b
Abhishek Katnoria <abhishekkatnoria993@gmail.com>
2024-08-06 23:31:46 +0530

    Added the assets and logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
-import { Button } from "@/components/ui/button";
+import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      Hello World 
-      <Button>Click me</Button>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            width={100}
+            height={100}
+            alt=""
+            src="/assets/icons/logo-full.svg"
+            className="w-fit h-10 mb-12"
+          />
+
+          <PatientForm />
+        </div>
+      </section>
     </div>
   );
 }
---
+++ components/forms/PatientForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PatientForm = () => {
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
+
+export default PatientForm;
